175b14d1eb7d596d81d3bd5a907c9d766a691bfa
prabhas <prabhasonteru123@gmail.com>
2024-03-30 21:42:46 +0530

    added plots

--- pruning/structured/decay_pruning.py
+++ pruning/structured/decay_pruning.py
@@ -14,7 +14,17 @@ from pruning.Train import Trainer
 
 
 class DecayPrune:
-    def __init__(self, model, epochs, train_loader, criterion, optimizer, pruning_rate=0.5,decay=0.1,reverse=False):
+    def __init__(self, model=None, epochs=None, train_loader=None, criterion=None, optimizer=None, pruning_rate=0.5,decay=0.1,reverse=False):
+        self.model = model
+        self.pruning_rate = pruning_rate
+        self.optimizer = optimizer
+        self.epochs = epochs
+        self.train_loader = train_loader
+        self.criterion = criterion
+        self.reverse = reverse
+        self.decay = decay
+        
+    def setargs(self, model, epochs, train_loader, criterion, optimizer, pruning_rate=0.5,decay=0.1,reverse=False):
         self.model = model
         self.pruning_rate = pruning_rate
         self.optimizer = optimizer
@@ -28,14 +38,15 @@ class DecayPrune:
         # prune the model and return it.
         model = copy.deepcopy(self.model)
         if self.reverse:
-            self.pruning_rate -= (len(list(model.parameters()))-1) * self.decay
-            for name, module in model.named_modules():
+            # self.pruning_rate = (len(list(self.model.parameters()))-1) * self.decay
+            i=1
+            for name, module in reversed(list(model.named_modules())):
                 if isinstance(module, torch.nn.Conv2d) or isinstance(module, torch.nn.Linear):
                     prune.l1_unstructured(module, name='weight', amount=self.pruning_rate)
                     prune.remove(module, name='weight')
-                    self.pruning_rate = self.pruning_rate + self.decay #Increasing decay rate as layers go deeper
-                    if self.pruning_rate >= 1.0:
-                        self.pruning_rate= 0.995 #To avoid over flows
+                    self.pruning_rate = self.pruning_rate - self.decay #Increasing decay rate as layers go deeper
+                    if self.pruning_rate<= 0.0:
+                        self.pruning_rate = 0.05 #To avoid under flows
         else:
             for name, module in model.named_modules():
                 if isinstance(module, torch.nn.Conv2d) or isinstance(module, torch.nn.Linear):
